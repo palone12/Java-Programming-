83128b64e85a87fdd8a10c1f74293c9e3a0f9304
Pranay Alone <64677840+palone12@users.noreply.github.com>
2021-09-23 23:26:32 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,10 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class for_loop {
+    public  static void main(String[]args){
+        for (int counter =0; counter<3 ;counter= counter + 1){
+            System.out.println("Hello World");
+
+        }
+    }
+}
---
+++ forloopq1.java
@@ -0,0 +1,7 @@
+public class forloopq1 {
+    public static void main(String[]args){
+        for (int a=0;a<=10;a= a+1){
+            System.out.println(a);
+        }
+    }
+}
